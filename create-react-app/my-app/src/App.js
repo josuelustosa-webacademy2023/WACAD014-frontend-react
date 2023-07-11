593039517c7204712b5cc2af4adde47a53385ecd
Josué Lustosa <josuelustosa.job@gmail.com>
2023-07-11 19:48:42 -0400

    feat: criando projeto com create-react-app e criando componentes

---
+++ create-react-app/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Table from "./Table/Table";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container text-center">
+        <h1>Olá, React!</h1>
+        <Table />
+      </div>
+    );
+  }
+}
+
+export default App;
